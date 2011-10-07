999580cfd73b222315275b53517918b10c078ff2
Sylvain Didelot <sylvain.didelot@exascale-computing.eu>
2011-10-07 03:15:15 +0200

    MPC: fix bug with LD_LIBRARY_PRELOAD + missing files

---
+++ mpc/MPC_Common/sctk_asm/sctk_atomics.c
@@ -0,0 +1,43 @@
+/* ############################# MPC License ############################## */
+/* # Wed Nov 19 15:19:19 CET 2008                                         # */
+/* # Copyright or (C) or Copr. Commissariat a l'Energie Atomique          # */
+/* #                                                                      # */
+/* # IDDN.FR.001.230040.000.S.P.2007.000.10000                            # */
+/* # This file is part of the MPC Runtime.                                # */
+/* #                                                                      # */
+/* # This software is governed by the CeCILL-C license under French law   # */
+/* # and abiding by the rules of distribution of free software.  You can  # */
+/* # use, modify and/ or redistribute the software under the terms of     # */
+/* # the CeCILL-C license as circulated by CEA, CNRS and INRIA at the     # */
+/* # following URL http://www.cecill.info.                                # */
+/* #                                                                      # */
+/* # The fact that you are presently reading this means that you have     # */
+/* # had knowledge of the CeCILL-C license and that you accept its        # */
+/* # terms.                                                               # */
+/* #                                                                      # */
+/* # Authors:                                                             # */
+/* #   - PULVERAIL Sebastien sebastien.pulverail@sogeti.com               # */
+/* #   - COTTRET Maxime maxime.cottret@sogeti.com                         # */
+/* #                                                                      # */
+/* ######################################################################## */
+#include "sctk_atomics.h"
+
+#include <sys/time.h>
+
+/*! \brief Relinquish the CPU moving the current thread at the end of the queue
+ *
+ */
+void sctk_atomics_pause()
+{
+	OPA_busy_wait();
+}
+
+/*! \brief Get the timestamp
+ *
+ */
+double sctk_atomics_get_timestamp()
+{
+	struct timeval tp;
+	gettimeofday(&tp, NULL);
+	return tp.tv_usec + tp.tv_sec * 1000000;
+}
