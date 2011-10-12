183b64c5858ba27433c4356c3eeddf285018459c
Sylvain Didelot <sylvain.didelot@exascale-computing.eu>
2011-10-12 15:33:02 +0200

    MPC: launch method bugfixes. Low_level_comm no more initialized when 1 process is involved

--- mpc/MPC_Common/sctk_launch/sctk_launch.c
+++ mpc/MPC_Common/sctk_launch/sctk_launch.c
@@ -468,9 +468,14 @@ sctk_get_verbosity ()
   static void
 sctk_use_network (char *arg)
 {
+  /* if the network mode is different to none,
+   * we initialize it. */
 #ifdef MPC_Message_Passing
-  sctk_network_mode = arg;
-  sctk_net_init_driver (arg);
+  if (strcmp(arg, "none"))
+  {
+    sctk_network_mode = arg;
+    sctk_net_init_driver (arg);
+  }
 #endif
 }
 
@@ -547,7 +552,6 @@ sctk_env_init_intern (int *argc, char ***argv)
   sctk_init ();
   sctk_initial_argc = *argc;
   init_argument = *argv;
-  sctk_pmi_init();
   sctk_topology_init ();
   sctk_print_version ("Init Launch", SCTK_LOCAL_VERSION_MAJOR,
       SCTK_LOCAL_VERSION_MINOR);
