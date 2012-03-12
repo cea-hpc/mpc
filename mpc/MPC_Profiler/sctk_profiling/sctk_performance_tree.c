a92ef8da7de237d64f27f916bb948b20b05ba886
Jean-Baptiste Besnard <jbbesnard@paratools.com>
2012-03-12 16:16:32 +0100

    MPC_Profiler : Missing files (ooops)

---
+++ mpc/MPC_Profiler/sctk_profiling/sctk_performance_tree.c
@@ -0,0 +1,174 @@
+/* ############################# MPC License ############################## */
+/* # Wed Nov 19 15:19:19 CET 2008                                         # */
+/* # Copyright or (C) or Copr. Commissariat a l'Energie Atomique          # */
+/* #                                                                      # */
+/* # IDDN.FR.001.230040.000.S.P.2007.000.10000                            # */
+/* # This file is part of the MPC Runtime.                                # */
+/* #                                                                      # */
+/* # This software is governed by the CeCILL-C license under French law   # */
+/* # and abiding by the rules of distribution of free software.  You can  # */
+/* # use, modify and/ or redistribute the software under the terms of     # */
+/* # the CeCILL-C license as circulated by CEA, CNRS and INRIA at the     # */
+/* # following URL http://www.cecill.info.                                # */
+/* #                                                                      # */
+/* # The fact that you are presently reading this means that you have     # */
+/* # had knowledge of the CeCILL-C license and that you accept its        # */
+/* # terms.                                                               # */
+/* #                                                                      # */
+/* # Authors:                                                             # */
+/* #   - BESNARD Jean-Baptiste jean-baptiste.besnard@cea.fr               # */
+/* #                                                                      # */
+/* ######################################################################## */
+
+#include "sctk_performance_tree.h"
+
+#include "sctk_debug.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+void sctk_performance_tree_relative_percentage( struct sctk_profiler_array *array, int id, int parent_id, int depth, void *arg )
+{
+	struct sctk_performance_tree *tr = (struct sctk_performance_tree *)arg;
+	
+	/* If entry has parent */
+	if( sctk_profiler_array_get_parent( id ) != SCTK_PROFILE_NO_PARENT )
+	{
+		/* Compute relative percentage */
+		if( sctk_profiler_array_get_time( array, parent_id ) )
+		{
+			tr->entry_relative_percentage_time[ id ] = (double)sctk_profiler_array_get_time( array, id ) / (double)sctk_profiler_array_get_time( array, parent_id );
+		}
+		else
+		{
+			tr->entry_relative_percentage_time[ id ] =  0;
+		}
+
+		if( sctk_profiler_array_get_hits( array, parent_id ) )
+		{
+			tr->entry_relative_percentage_hits[ id ] = (double)sctk_profiler_array_get_hits( array, id ) / (double)sctk_profiler_array_get_hits( array, parent_id );
+		}
+		else
+		{
+			tr->entry_relative_percentage_hits[ id ] =  0;
+		}
+		
+		if( 1 < tr->entry_relative_percentage_time[ id ] )
+		{
+			sctk_warning( "sctk_performance_tree detected an overflow in performance measurements");
+			tr->entry_relative_percentage_time[ id ] = 1;
+		}
+		
+		if( 1 < tr->entry_relative_percentage_hits[ id ] )
+		{
+			sctk_warning( "sctk_performance_tree detected an overflow in performance measurements");
+			tr->entry_relative_percentage_hits[ id ] = 1;
+		}
+		
+	}
+	else
+	{
+		uint64_t roots_hits = 0;
+		uint64_t roots_time = 0;
+		
+		int i = 0;
+		
+		for( i = 1 ; i < SCTK_PROFILE_KEY_COUNT ; i++ )
+		{
+			if( sctk_profiler_array_get_parent( i ) == SCTK_PROFILE_NO_PARENT )
+			{
+				roots_hits += sctk_profiler_array_get_hits( array, i );
+				roots_time += sctk_profiler_array_get_time( array, i );
+			}
+		}
+		
+		tr->entry_relative_percentage_time[ id ] = (double)sctk_profiler_array_get_time( array, id ) / roots_time;
+		tr->entry_relative_percentage_hits[ id ] = (double)sctk_profiler_array_get_hits( array, id ) / roots_hits;
+	}
+}
+
+
+void sctk_performance_tree_total_time_and_hits( struct sctk_profiler_array *array, int id, int parent_id, int depth, void *arg )
+{
+	struct sctk_performance_tree *tr = (struct sctk_performance_tree *)arg;
+	
+	if( !sctk_profiler_array_has_child( id ) )
+	{
+		/* Only process leaf nodes */
+		
+		tr->total_time += sctk_profiler_array_get_time( array, id );
+		tr->total_hits += sctk_profiler_array_get_hits( array, id );
+		
+	}
+
+
+}
+
+void sctk_performance_tree_absolute_percentage( struct sctk_profiler_array *array, int id, int parent_id, int depth, void *arg )
+{
+	struct sctk_performance_tree *tr = (struct sctk_performance_tree *)arg;
+
+
+	/* Compute relative percentage */
+	if( tr->total_time )
+	{
+		tr->entry_total_percentage_time[ id ] = (double)sctk_profiler_array_get_time( array, id ) / (double)tr->total_time;
+	}
+	else
+	{
+		tr->entry_total_percentage_time[ id ] =  0;
+	}
+	
+	if( tr->total_hits )
+	{
+		tr->entry_total_percentage_hits[ id ] = (double)sctk_profiler_array_get_hits( array, id ) / (double)tr->total_hits;
+	}
+	else
+	{
+		tr->entry_total_percentage_hits[ id ] =  0;
+	}
+}
+
+
+
+
+void  sctk_performance_tree_init( struct sctk_performance_tree *tr, struct sctk_profiler_array *array)
+{
+	if( !array->been_unified )
+	{
+		sctk_error( "Performance tree can be built only over an unified tree !");
+		abort();
+	}
+
+	memset( (void *)tr, 0, sizeof(  struct sctk_performance_tree ) );
+
+	/* Fill relative percentage */
+	sctk_profiler_array_walk( array, sctk_performance_tree_relative_percentage , (void *)tr , 1);
+	/* Fill Total time and hits */
+	sctk_profiler_array_walk( array, sctk_performance_tree_total_time_and_hits , (void *)tr , 1);
+	/* Computa absolute percentages */
+	sctk_profiler_array_walk( array, sctk_performance_tree_absolute_percentage , (void *)tr , 1);
+
+	int i = 0;
+
+	/* Fill average time */
+	for( i = 0 ; i  < SCTK_PROFILE_KEY_COUNT ; i++ )
+	{
+		if( sctk_profiler_array_get_hits( array, i ) )
+		{
+			tr->entry_average_time[ i ] = sctk_profiler_array_get_time( array, i ) / sctk_profiler_array_get_hits( array, i );
+		}
+		else
+		{
+			tr->entry_average_time[ i ] =  0;
+		}
+	}
+
+}
+
+
+void  sctk_performance_tree_release( struct sctk_performance_tree *tr )
+{
+	memset( (void *)tr, 0, sizeof(  struct sctk_performance_tree ) );
+}
+
---
+++ mpc/MPC_Profiler/sctk_profiling/sctk_profile_meta.h
@@ -0,0 +1,57 @@
+/* ############################# MPC License ############################## */
+/* # Wed Nov 19 15:19:19 CET 2008                                         # */
+/* # Copyright or (C) or Copr. Commissariat a l'Energie Atomique          # */
+/* #                                                                      # */
+/* # IDDN.FR.001.230040.000.S.P.2007.000.10000                            # */
+/* # This file is part of the MPC Runtime.                                # */
+/* #                                                                      # */
+/* # This software is governed by the CeCILL-C license under French law   # */
+/* # and abiding by the rules of distribution of free software.  You can  # */
+/* # use, modify and/ or redistribute the software under the terms of     # */
+/* # the CeCILL-C license as circulated by CEA, CNRS and INRIA at the     # */
+/* # following URL http://www.cecill.info.                                # */
+/* #                                                                      # */
+/* # The fact that you are presently reading this means that you have     # */
+/* # had knowledge of the CeCILL-C license and that you accept its        # */
+/* # terms.                                                               # */
+/* #                                                                      # */
+/* # Authors:                                                             # */
+/* #   - BESNARD Jean-Baptiste jean-baptiste.besnard@cea.fr               # */
+/* #                                                                      # */
+/* ######################################################################## */
+
+#ifndef SCTK_PROFILE_META_H
+#define SCTK_PROFILE_META_H
+
+#include <sys/time.h>
+#include <stdint.h>
+
+#include "sctk_spinlock.h"
+
+struct sctk_profile_meta
+{
+	int task_count;
+	int process_count;
+	int node_count;
+
+	uint64_t walltime;
+	double ticks_per_second;
+
+	char command[1000];
+
+	uint64_t begin_ts;
+	struct timeval calibration_time;
+
+	sctk_spinlock_t lock;
+	int status;
+};
+
+
+void sctk_profile_meta_init(struct sctk_profile_meta *meta);
+void sctk_profile_meta_release(struct sctk_profile_meta *meta);
+
+
+void sctk_profile_meta_begin_compute(struct sctk_profile_meta *meta);
+void sctk_profile_meta_end_compute(struct sctk_profile_meta *meta);
+
+#endif /* SCTK_PROFILE_META_H */
