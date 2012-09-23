93d299bd09de44f7f84182118658c2181f138a4e
Sebastien Valat <sebastien.valat@cea.fr>
2012-09-23 22:58:29 +0200

    MPC_Allocator: Opps missing new files : sctk_alloc_config.[c|h]

---
+++ mpc/MPC_Allocator/src/sctk_alloc_config.h
@@ -0,0 +1,60 @@
+/* ############################# MPC License ############################## */
+/* # Wed Nov 19 15:19:19 CET 2008                                         # */
+/* # Copyright or (C) or Copr. Commissariat a l'Energie Atomique          # */
+/* #                                                                      # */
+/* # IDDN.FR.001.230040.000.S.P.2007.000.10000                            # */
+/* # This file is part of the MPC Runtime.                                # */
+/* #                                                                      # */
+/* # This software is governed by the CeCILL-C license under French law   # */
+/* # and abiding by the rules of distribution of free software.  You can  # */
+/* # use, modify and/ or redistribute the software under the terms of     # */
+/* # the CeCILL-C license as circulated by CEA, CNRS and INRIA at the     # */
+/* # following URL http://www.cecill.info.                                # */
+/* #                                                                      # */
+/* # The fact that you are presently reading this means that you have     # */
+/* # had knowledge of the CeCILL-C license and that you accept its        # */
+/* # terms.                                                               # */
+/* #                                                                      # */
+/* # Authors:                                                             # */
+/* #   - Valat Sébastien sebastien.valat@cea.fr                           # */
+/* #                                                                      # */
+/* ######################################################################## */
+
+#ifndef SCTK_ALLOC_CONFIG_H
+#define SCTK_ALLOC_CONFIG_H
+
+/************************** HEADERS ************************/
+#include "sctk_alloc_common.h"
+
+//select the config header (with or without MPC)
+#ifdef MPC_Common
+#include "sctk_runtime_config.h"
+#else //MPC_Common
+#include "../../MPC_Config/generated/sctk_runtim_config_struct.h"
+#endif //MPC_Common
+
+/************************* GLOBALS *************************/
+#ifndef MPC_Common
+extern struct sctk_runtime_config_struct_allocator sctk_alloc_global_config;
+#endif //MPC_Common
+
+/************************* FUNCTION ************************/
+/**
+ * Wrapper to get quick access to the allocator configuration structure.
+ * It manage the case with or without MPC to ensure usability out of MPC.
+**/
+static __inline__ const struct sctk_runtime_config_struct_allocator * sctk_alloc_config(void)
+{
+	#ifdef MPC_Common
+	return &sctk_runtime_config_get_nocheck()->modules.allocator;
+	#else //MPC_Common
+	return &sctk_alloc_global_config;
+	#endif //MPC_Common
+}
+
+/************************* FUNCTION ************************/
+void sctk_alloc_config_init(void);
+void sctk_alloc_config_egg_init(void);
+SCTK_STATIC void sctk_alloc_config_init_static_defaults(struct sctk_runtime_config_struct_allocator * config);
+
+#endif //SCTK_ALLOC_CONFIG_H
