18c651553fd32c66beecb16f7059c0dc0491df96
Sylvain Didelot <sylvain.didelot@exascale-computing.eu>
2013-01-18 15:08:46 +0100

    MPC: Bugfix for the last wrong commit (MPC was not compiling...)

--- mpc/MPC_Message_Passing/sctk_driver_ib/sctk_ib_mpi.c
+++ mpc/MPC_Message_Passing/sctk_driver_ib/sctk_ib_mpi.c
@@ -388,7 +388,7 @@ sctk_network_notify_perform_message_ib (int remote_process, int remote_task_id, 
   POLL_INIT(&poll);
   sctk_network_poll_all_cq(rail, &poll);
   POLL_INIT(&poll);
-  sctk_ib_cp_poll(rail, &poll, polling_task_id, 1);
+  sctk_ib_cp_poll(rail, &poll, polling_task_id);
 }
 
 static void
@@ -398,7 +398,7 @@ sctk_network_notify_idle_message_ib (sctk_rail_info_t* rail){
   struct sctk_ib_polling_s poll;
 
   if (config->ibv_steal > 0) {
-    sctk_ib_cp_steal(rail, &poll);
+    sctk_ib_cp_steal(rail, &poll, 1);
   }
 }
 
@@ -411,7 +411,7 @@ sctk_network_notify_any_source_message_ib (int polling_task_id, sctk_rail_info_t
   POLL_INIT(&poll);
   sctk_network_poll_all_cq(rail, &poll);
   POLL_INIT(&poll);
-  sctk_ib_cp_poll(rail, &poll, polling_task_id, 1);
+  sctk_ib_cp_poll(rail, &poll, polling_task_id);
 }
 
 static void
