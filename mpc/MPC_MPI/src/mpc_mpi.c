ba912b03b094f3aed3fa299828e3c160edd336af
Marc Perache <marc.perache@cea.fr>
2014-01-26 16:16:45 +0100

    Modify error handler in MPI_Allreduce and intercommunicators

--- mpc/MPC_MPI/src/mpc_mpi.c
+++ mpc/MPC_MPI/src/mpc_mpi.c
@@ -9321,6 +9321,11 @@ PMPI_Allreduce (void *sendbuf, void *recvbuf, int count,
 {
   int res = MPI_ERR_INTERN;
   mpi_check_comm (comm, comm);
+
+  if(sctk_is_inter_comm (comm)){
+    MPI_ERROR_REPORT(comm,MPI_ERR_COMM,"");
+  }
+
   mpi_check_buf (sendbuf, comm);
   mpi_check_buf (recvbuf, comm);
   mpi_check_count (count, comm);
