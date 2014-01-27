c4dac11343ba6d841a7e3ffa48c7b9387bbd3afb
Marc Perache <marc.perache@cea.fr>
2014-01-27 21:31:43 +0100

    Remove small bug with MPI oversubscription

--- mpc/MPC_Threads/sctk_thread/sctk_thread.c
+++ mpc/MPC_Threads/sctk_thread/sctk_thread.c
@@ -2063,7 +2063,7 @@ sctk_get_init_vp_and_nbvp (int i, int *nbVp)
 	}
       first = last + 1;
       j++;
-      if (cpu_nb % task_nb > j)
+      if ((cpu_nb % task_nb > j) && (cpu_nb > task_nb))
 	   proc++;
     }
   sctk_nodebug ("sctk_get_init_vp: (After loop) Put task %d on VP %d", i, proc);
