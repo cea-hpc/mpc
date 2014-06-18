1389b8294a1855e602bbc67481b6eaef7000f3eb
Augustin Serraz <augustin.serraz@exascale-computing.eu>
2014-06-18 13:57:59 +0200

    afficher tailles tls written

--- mpcframework/MPC_Threads/sctk_thread/sctk_tls.c
+++ mpcframework/MPC_Threads/sctk_thread/sctk_tls.c
@@ -252,8 +252,9 @@ sctk_get_module_file_decr (size_t m, size_t module_size)
       memset (tls_module, 0, module_size);
       sctk_init_module (m, tls_module, module_size);
 	
-      /* assume(write (fd, tls_module, module_size) == module_size); */
-      res_write = write (fd, tls_module, module_size);
+	  size = write (fd, tls_module, module_size);
+	  fprintf(stderr, "size = %d, size_written = %d\n", module_size, size);
+      assume(size == module_size);
       free (tls_module);
       remove (name);
       if(res_write != module_size){
