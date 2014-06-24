16c4aea43c277a0bb29a28f54c272dfaacef2908
Augustin Serraz <augustin.serraz@exascale-computing.eu>
2014-06-24 16:24:52 +0200

    Update on COW

--- mpcframework/MPC_Threads/sctk_thread/sctk_tls.c
+++ mpcframework/MPC_Threads/sctk_thread/sctk_tls.c
@@ -252,9 +252,7 @@ sctk_get_module_file_decr (size_t m, size_t module_size)
       memset (tls_module, 0, module_size);
       sctk_init_module (m, tls_module, module_size);
 	
-	  size = write (fd, tls_module, module_size);
-	  fprintf(stderr, "fd = %d, size = %d, size_written = %d\n", fd, module_size, size);
-      assume(size == module_size);
+	  assume(write (fd, tls_module, module_size) == module_size);
       free (tls_module);
       remove (name);
       if(res_write != module_size){
