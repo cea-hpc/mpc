a103b40d2d5b5e04cae8619de6e4b647eaba8dd2
Jean-Baptiste Besnard <jbbesnard@paratools.com>
2014-11-17 11:29:49 -0500

    EXTERN32 : File Creation Only 	-> Prepare for extern32 rep implementation

---
+++ mpcframework/MPC_Message_Passing/mpc/mpc_extern32.h
@@ -0,0 +1,43 @@
+/* ############################# MPC License ############################## */
+/* # Wed Nov 19 15:19:19 CET 2008                                         # */
+/* # Copyright or (C) or Copr. Commissariat a l'Energie Atomique          # */
+/* #                                                                      # */
+/* # IDDN.FR.001.230040.000.S.P.2007.000.10000                            # */
+/* # This file is part of the MPC Runtime.                                # */
+/* #                                                                      # */
+/* # This software is governed by the CeCILL-C license under French law   # */
+/* # and abiding by the rules of distribution of free software.  You can  # */
+/* # use, modify and/ or redistribute the software under the terms of     # */
+/* # the CeCILL-C license as circulated by CEA, CNRS and INRIA at the     # */
+/* # following URL http://www.cecill.info.                                # */
+/* #                                                                      # */
+/* # The fact that you are presently reading this means that you have     # */
+/* # had knowledge of the CeCILL-C license and that you accept its        # */
+/* # terms.                                                               # */
+/* #                                                                      # */
+/* # Authors:                                                             # */
+/* #   - BESNARD Jean-Baptiste jbbesnard@paratools.fr                     # */
+/* #                                                                      # */
+/* ######################################################################## */
+
+#ifndef MPC_EXTERN32_H
+#define MPC_EXTERN32_H
+
+#include "mpcmp.h"
+#include "mpc_common.h"
+
+/* This file defines the extern32 data-rep following
+ * MPI standard 1.3: 13.5.2 External Data Representation: “external32”
+ * page 534. We start by defining all the conversion functions
+ * before applying them to flattened representations
+ * 
+ * Note that user defined data-rep are handled in ROMIO*/
+
+
+
+
+
+
+
+
+#endif
