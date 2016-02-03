f707f3cef0472194f356579fe3b9ac1a885f1023
Julien Jaeger <julien.jaeger@cea.fr>
2016-02-03 14:16:04 +0100

    quickfix for openmp thread placement bug in MPI+OpenMP

--- mpcframework/MPC_OpenMP/src/mpcomp_tree.c
+++ mpcframework/MPC_OpenMP/src/mpcomp_tree.c
@@ -794,9 +794,7 @@ __mpcomp_build_tree( mpcomp_instance_t * instance, int n_leaves, int depth, int 
 			 int res;
 
 			 sctk_thread_attr_init(&__attr);
-			 sctk_thread_attr_setbinding (& __attr, 
-			     mpcomp_get_global_index_from_cpu(
-			       instance->topology, target_vp) );
+             sctk_thread_attr_setbinding (& __attr, sctk_get_cpu() + target_vp);
 			 sctk_thread_attr_setstacksize (&__attr, 
 			     mpcomp_global_icvs.stacksize_var);
 
