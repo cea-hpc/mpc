8d5bdb57c3e61908689ed27812fc79c49a3a0860
Ricardo Bispo vieira <ricardo.bispo-vieira@exascale-computing.eu>
2016-02-15 17:03:51 +0100

    Bug fixing on OpenMP fortran header

--- mpcframework/MPC_OpenMP/include/omp_lib_kinds.h
+++ mpcframework/MPC_OpenMP/include/omp_lib_kinds.h
@@ -8,6 +8,9 @@
         parameter ( omp_sched_kind = selected_int_kind( 8 ) )
         integer ( omp_sched_kind ) omp_sched_static
         parameter ( omp_sched_static = 1 )
-        integer ( omp_sched_kind ) omp_sched_dynamic parameter ( omp_sched_dynamic = 2 )
-        integer ( omp_sched_kind ) omp_sched_guided parameter ( omp_sched_guided = 3 )
-        integer ( omp_sched_kind ) omp_sched_auto parameter ( omp_sched_auto = 4 )
+        integer ( omp_sched_kind ) omp_sched_dynamic
+        parameter ( omp_sched_dynamic = 2 )
+        integer ( omp_sched_kind ) omp_sched_guided
+        parameter ( omp_sched_guided = 3 )
+        integer ( omp_sched_kind ) omp_sched_auto
+        parameter ( omp_sched_auto = 4 )
