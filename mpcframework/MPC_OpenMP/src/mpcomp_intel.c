51b68b06b735a5d216c9af54509d282805ee57db
Ricardo Bispo vieira <ricardo.bispo-vieira@exascale-computing.eu>
2016-04-12 13:50:35 +0200

    Adding support for intel openmp user locks

--- mpcframework/MPC_OpenMP/src/mpcomp_intel.c
+++ mpcframework/MPC_OpenMP/src/mpcomp_intel.c
@@ -2670,149 +2670,137 @@ not_implemented() ;
 /*
  * Lock structure
  */
-/*typedef struct omp_lock_t {
+typedef struct omp_lock_t {
   void* lk;
-} iomp_lock_t;*/
+} iomp_lock_t;
 
 /*
- * Lock interface routines (fast versions with gtid passed in)
- */
-void 
+ *  * Lock interface routines (fast versions with gtid passed in)
+ *   */
+void
 __kmpc_init_lock( ident_t *loc, kmp_int32 gtid,  void **user_lock )
 {
   sctk_debug( "[%d] __kmpc_init_lock: "
       "Addr %p",
       ((mpcomp_thread_t *) sctk_openmp_thread_tls)->rank, user_lock ) ;
 
-  not_implemented() ;
-/*  if( user_lock == NULL ) {
+  if( user_lock == NULL ) {
     sctk_error("__kmpc_init_lock: Null lock reference");
     sctk_abort();
   }
 
   ((iomp_lock_t*) user_lock)->lk = (void*) sctk_malloc( sizeof( omp_lock_t ));
-  omp_init_lock( (omp_lock_t *) ((iomp_lock_t*) user_lock)->lk );*/
+  omp_init_lock( (omp_lock_t *) ((iomp_lock_t*) user_lock)->lk );
 }
 
-void 
+void
 __kmpc_init_nest_lock( ident_t *loc, kmp_int32 gtid, void **user_lock )
 {
-  not_implemented() ;
-/*  if( user_lock == NULL ) {
-    sctk_error("__kmpc_init_lock: Null lock reference");
+  if( user_lock == NULL ) {
+    sctk_error("__kmpc_init_nest_lock: Null lock reference");
     sctk_abort();
   }
 
   ((iomp_lock_t*) user_lock)->lk = (void*) sctk_malloc( sizeof( omp_nest_lock_t ));
-  omp_init_nest_lock( (omp_nest_lock_t*) ((iomp_lock_t*) user_lock)->lk );*/
+  omp_init_nest_lock( (omp_nest_lock_t*) ((iomp_lock_t*) user_lock)->lk );
 }
 
-void 
+void
 __kmpc_destroy_lock( ident_t *loc, kmp_int32 gtid, void **user_lock )
 {
-  not_implemented() ;
-/*  iomp_lock_t* llk;
+  omp_lock_t* llk;
 
   if( user_lock == NULL ) {
-    sctk_error("__kmpc_init_lock: Null lock reference");
+    sctk_error("__kmpc_destroy_lock: Null lock reference");
     sctk_abort();
   }
 
   llk = ((iomp_lock_t*) user_lock)->lk;
-  omp_destroy_lock( (omp_lock_t*) llk );
+  omp_destroy_lock( llk );
   free( llk );
-  llk = NULL;*/
+  llk = NULL;
 }
 
-void 
+void
 __kmpc_destroy_nest_lock( ident_t *loc, kmp_int32 gtid, void **user_lock )
 {
-  not_implemented() ;
-/*  iomp_lock_t* llk;
+  omp_nest_lock_t* llk;
 
   if( user_lock == NULL ) {
-    sctk_error("__kmpc_init_lock: Null lock reference");
+    sctk_error("__kmpc_destroy_nest_lock: Null lock reference");
     sctk_abort();
   }
 
   llk = ((iomp_lock_t*) user_lock)->lk;
-  omp_destroy_nest_lock( (omp_nest_lock_t*) llk );
+  omp_destroy_nest_lock( llk );
   free( llk );
-  llk = NULL;*/
+  llk = NULL;
 }
 
 void
 __kmpc_set_lock( ident_t *loc, kmp_int32 gtid, void **user_lock )
 {
-  not_implemented() ;
-  /*if( user_lock == NULL ) {
-    sctk_error("__kmpc_init_lock: Null lock reference");
+  if( user_lock == NULL ) {
+    sctk_error("__kmpc_set_lock: Null lock reference");
     sctk_abort();
   }
 
-  omp_set_lock( (omp_lock_t*) ((iomp_lock_t*) user_lock)->lk );*/
+  omp_set_lock( (omp_lock_t*) ((iomp_lock_t*) user_lock)->lk );
 }
 
-void 
+void
 __kmpc_set_nest_lock( ident_t *loc, kmp_int32 gtid, void **user_lock )
 {
-  not_implemented() ;
-  /*if( user_lock == NULL ) {
-    sctk_error("__kmpc_init_lock: Null lock reference");
+  if( user_lock == NULL ) {
+    sctk_error("__kmpc_set_nest_lock: Null lock reference");
     sctk_abort();
   }
 
-  omp_set_nest_lock( (omp_nest_lock_t*) ((iomp_lock_t*) user_lock)->lk );*/
+  omp_set_nest_lock( (omp_nest_lock_t*) ((iomp_lock_t*) user_lock)->lk );
 }
 
-void 
+void
 __kmpc_unset_lock( ident_t *loc, kmp_int32 gtid, void **user_lock )
 {
-  not_implemented() ;
-/*  if( user_lock == NULL ) {
-    sctk_error("__kmpc_init_lock: Null lock reference");
+  if( user_lock == NULL ) {
+    sctk_error("__kmpc_unset_lock: Null lock reference");
     sctk_abort();
   }
 
-  omp_unset_lock( (omp_lock_t*) ((iomp_lock_t*) user_lock)->lk );*/
+  omp_unset_lock( (omp_lock_t*) ((iomp_lock_t*) user_lock)->lk );
 }
 
-void 
+void
 __kmpc_unset_nest_lock( ident_t *loc, kmp_int32 gtid, void **user_lock )
 {
-  not_implemented() ;
-/*  if( user_lock == NULL ) {
-    sctk_error("__kmpc_init_lock: Null lock reference");
+  if( user_lock == NULL ) {
+    sctk_error("__kmpc_unset_nest_lock: Null lock reference");
     sctk_abort();
   }
 
-  omp_unset_nest_lock( (omp_nest_lock_t*) ((iomp_lock_t*) user_lock)->lk );*/
+  omp_unset_nest_lock( (omp_nest_lock_t*) ((iomp_lock_t*) user_lock)->lk );
 }
 
-int 
+int
 __kmpc_test_lock( ident_t *loc, kmp_int32 gtid, void **user_lock )
 {
-  not_implemented() ;
-/*
   if( user_lock == NULL ) {
-    sctk_error("__kmpc_init_lock: Null lock reference");
+    sctk_error("__kmpc_test_lock: Null lock reference");
     sctk_abort();
   }
 
-  return ( omp_test_lock( (omp_lock_t*) ((iomp_lock_t*) user_lock)->lk ) == 0 );*/
+  return omp_test_lock( (omp_lock_t*) ((iomp_lock_t*) user_lock)->lk );
 }
 
-int 
+int
 __kmpc_test_nest_lock( ident_t *loc, kmp_int32 gtid, void **user_lock )
 {
-  not_implemented() ;
-/*  if( user_lock == NULL ) {
-    sctk_error("__kmpc_init_lock: Null lock reference");
+  if( user_lock == NULL ) {
+    sctk_error("__kmpc_test_nest_lock: Null lock reference");
     sctk_abort();
   }
-  return ( omp_test_nest_lock( (omp_nest_lock_t*) ((iomp_lock_t*) user_lock)->lk ) == 0 );*/
+  return omp_test_nest_lock( (omp_nest_lock_t*) ((iomp_lock_t*) user_lock)->lk );
 }
-
 
 
 
