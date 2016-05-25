761fd6114272c46fbb391f91df44ff759439b347
Julien Adam <adamj@paratools.com>
2016-05-25 14:24:25 +0200

    TLS: New tarball + add a function to retrieve the core associated to a PU + renaming wait() fn

--- mpcframework/MPC_Common/sctk_topology/sctk_topology.c
+++ mpcframework/MPC_Common/sctk_topology/sctk_topology.c
@@ -417,6 +417,18 @@ int sctk_topology_convert_os_pu_to_logical( int pu_os_id )
 	return pu->logical_index;
 }
 
+int sctk_topology_get_logical_pu_ancestor( int pu_id)
+{
+	hwloc_obj_t target_pu = hwloc_get_obj_by_type(topology,HWLOC_OBJ_PU, pu_id );	
+	if( !target_pu )
+	{
+		/* Assume 0 in case of error */
+		return 0;
+	}
+	
+	return target_pu->parent->os_index;	
+}
+
 int sctk_topology_convert_logical_pu_to_os( int pu_id )
 {
 	hwloc_obj_t target_pu = hwloc_get_obj_by_type(topology,HWLOC_OBJ_PU, pu_id );
--- mpcframework/MPC_Threads/sctk_thread/sctk_thread.c
+++ mpcframework/MPC_Threads/sctk_thread/sctk_thread.c
@@ -762,7 +762,7 @@ sctk_thread_create (sctk_thread_t * restrict __threadp,
   *cur_tx = malloc(sizeof(extls_ctx_t));
   extls_ctx_herit(old_ctx, *cur_tx, LEVEL_TASK);
   extls_ctx_restore(*cur_tx);
-  extls_ctx_bind(*cur_tx, tmp->bind_to);
+  extls_ctx_bind(*cur_tx,sctk_topology_get_logical_pu_ancestor(tmp->bind_to) );
 #endif
 
   res = __sctk_ptr_thread_create (__threadp, __attr, (void *(*)(void *))
@@ -1950,7 +1950,7 @@ sctk_thread_timed_wait_for_value (volatile int *data, int value, unsigned int ma
 	return 0;
 }
 
-void extls_hls_wait_for_value(volatile int *data, int value)
+void extls_wait_for_value(volatile int *data, int value)
 {
   __sctk_ptr_thread_wait_for_value_and_poll (data, value, NULL, NULL);
 }
