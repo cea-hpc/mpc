d47ede4162532915a19001a0b1e09a3c82bbceb3
Julien Adam <adamj@paratools.com>
2016-06-23 14:06:10 +0200

    TLS: fix forgotten sctk_extls reference in MPC_Debugger

--- mpcframework/MPC_Debugger/sctk_dbg_thread/sctk_thread_dbg.c
+++ mpcframework/MPC_Debugger/sctk_dbg_thread/sctk_thread_dbg.c
@@ -98,7 +98,7 @@ int sctk_init_thread_debug (sctk_thread_data_t *item) {
   rtdb_set_thread_stksize (thread, tid->stack_size) ;
   rtdb_set_thread_type (thread, TD_THR_USER) ;
 
-  rtdb_set_thread_extls(thread,tid->ctx.sctk_extls);
+  rtdb_set_thread_extls(thread,tid->ctx.tls_ctx);
 
   sctk_nodebug("context");
 #if defined(TDB_i686_ARCH_TDB) || defined(TDB_x86_64_ARCH_TDB)
