b67abbe631b673151344a1d57a1b3d6142450f96
Marc Perache <marc.perache@cea.fr>
2016-08-01 13:32:49 +0200

    MPC_Profiler: add sctk_profiler_keys_mpi_nbc.h

---
+++ mpcframework/MPC_Profiler/src/sctk_profiler_keys_mpi_nbc.h
@@ -0,0 +1,54 @@
+PROBE(MPI_NBC, MPI, MPI Non Blocking Collectives)
+PROBE(MPI_Iallreduce, MPI_NBC, MPI_Iallreduce)                       // LINE 25
+PROBE(MPI_Ibarrier, MPI_NBC, MPI_Ibarrier)                           // LINE 26
+PROBE(MPI_Ibcast, MPI_NBC, MPI_Ibcast)                               // LINE 27
+PROBE(MPI_Igather, MPI_NBC, MPI_Igather)                             // LINE 28
+PROBE(MPI_Igatherv, MPI_NBC, MPI_Igatherv)                           // LINE 29
+PROBE(MPI_Iscatter, MPI_NBC, MPI_Iscatter)                           // LINE 30
+PROBE(MPI_Iscatterv, MPI_NBC, MPI_Iscatterv)                         // LINE 31
+PROBE(MPI_Iallgather, MPI_NBC, MPI_Iallgather)                       // LINE 32
+PROBE(MPI_Iallgatherv, MPI_NBC, MPI_Iallgatherv)                     // LINE 33
+PROBE(MPI_Ialltoall, MPI_NBC, MPI_Ialltoall)                         // LINE 34
+PROBE(MPI_Ialltoallv, MPI_NBC, MPI_Ialltoallv)                       // LINE 35
+PROBE(MPI_Ialltoallw, MPI_NBC, MPI_Ialltoallw)                       // LINE 36
+PROBE(MPI_Ireduce, MPI_NBC, MPI_Ireduce)                             // LINE 37
+PROBE(MPI_Ireduce_scatter, MPI_NBC, MPI_Ireduce_scatter)             // LINE 38
+PROBE(MPI_Ireduce_scatter_block, MPI_NBC, MPI_Ireduce_scatter_block) // LINE 39
+PROBE(MPI_Iscan, MPI_NBC, MPI_Iscan)                                 // LINE 40
+PROBE(MPI_Iexscan, MPI_NBC, MPI_Iexscan)                             // LINE 41
+PROBE(mpi_iallreduce_, MPI_NBC, mpi_iallreduce_)                     // LINE 43
+PROBE(mpi_ibarrier_, MPI_NBC, mpi_ibarrier_)                         // LINE 44
+PROBE(mpi_ibcast_, MPI_NBC, mpi_ibcast_)                             // LINE 45
+PROBE(mpi_igather_, MPI_NBC, mpi_igather_)                           // LINE 46
+PROBE(mpi_igatherv_, MPI_NBC, mpi_igatherv_)                         // LINE 47
+PROBE(mpi_iscatter_, MPI_NBC, mpi_iscatter_)                         // LINE 48
+PROBE(mpi_iscatterv_, MPI_NBC, mpi_iscatterv_)                       // LINE 49
+PROBE(mpi_iallgather_, MPI_NBC, mpi_iallgather_)                     // LINE 50
+PROBE(mpi_iallgatherv_, MPI_NBC, mpi_iallgatherv_)                   // LINE 51
+PROBE(mpi_ialltoall_, MPI_NBC, mpi_ialltoall_)                       // LINE 52
+PROBE(mpi_ialltoallv_, MPI_NBC, mpi_ialltoallv_)                     // LINE 53
+PROBE(mpi_ialltoallw_, MPI_NBC, mpi_ialltoallw_)                     // LINE 54
+PROBE(mpi_ireduce_, MPI_NBC, mpi_ireduce_)                           // LINE 55
+PROBE(mpi_ireduce_scatter_, MPI_NBC, mpi_ireduce_scatter_)           // LINE 56
+PROBE(mpi_ireduce_scatter_block_, MPI_NBC, mpi_ireduce_scatter_block_) // LINE
+                                                                       // 57
+PROBE(mpi_iscan_, MPI_NBC, mpi_iscan_)                       // LINE 58
+PROBE(mpi_iexscan_, MPI_NBC, mpi_iexscan_)                   // LINE 59
+PROBE(mpi_iallreduce__, MPI_NBC, mpi_iallreduce__)           // LINE 61
+PROBE(mpi_ibarrier__, MPI_NBC, mpi_ibarrier__)               // LINE 62
+PROBE(mpi_ibcast__, MPI_NBC, mpi_ibcast__)                   // LINE 63
+PROBE(mpi_igather__, MPI_NBC, mpi_igather__)                 // LINE 64
+PROBE(mpi_igatherv__, MPI_NBC, mpi_igatherv__)               // LINE 65
+PROBE(mpi_iscatter__, MPI_NBC, mpi_iscatter__)               // LINE 66
+PROBE(mpi_iscatterv__, MPI_NBC, mpi_iscatterv__)             // LINE 67
+PROBE(mpi_iallgather__, MPI_NBC, mpi_iallgather__)           // LINE 68
+PROBE(mpi_iallgatherv__, MPI_NBC, mpi_iallgatherv__)         // LINE 69
+PROBE(mpi_ialltoall__, MPI_NBC, mpi_ialltoall__)             // LINE 70
+PROBE(mpi_ialltoallv__, MPI_NBC, mpi_ialltoallv__)           // LINE 71
+PROBE(mpi_ialltoallw__, MPI_NBC, mpi_ialltoallw__)           // LINE 72
+PROBE(mpi_ireduce__, MPI_NBC, mpi_ireduce__)                 // LINE 73
+PROBE(mpi_ireduce_scatter__, MPI_NBC, mpi_ireduce_scatter__) // LINE 74
+PROBE(mpi_ireduce_scatter_block__, MPI_NBC,
+      mpi_ireduce_scatter_block__)           // LINE 75
+PROBE(mpi_iscan__, MPI_NBC, mpi_iscan__)     // LINE 76
+PROBE(mpi_iexscan__, MPI_NBC, mpi_iexscan__) // LINE 77
