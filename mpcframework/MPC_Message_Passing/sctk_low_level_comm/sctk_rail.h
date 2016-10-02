3d5590362d08cbeb6226b1d8b58689fcb369f54d
Marc Perache <marc.perache@cea.fr>
2016-10-02 17:05:49 +0200

    Remove error in Portals4

--- mpcframework/MPC_Message_Passing/sctk_low_level_comm/sctk_rail.h
+++ mpcframework/MPC_Message_Passing/sctk_low_level_comm/sctk_rail.h
@@ -46,8 +46,8 @@ typedef union
 	sctk_tcp_rail_info_t tcp;	/**< TCP Rail Info */
 	sctk_ib_rail_info_t ib;		/**< IB Rail Info */
 	sctk_shm_rail_info_t shm;	/**< SHM Rail Info */
-#ifdef MPC_USE_PORTAL
-	sctk_portals_rail_info_t portals; /**< Portals Info */
+#ifdef MPC_USE_PORTALS
+        sctk_portals_rail_info_t portals; /**< Portals Info */
 #endif
 	sctk_topological_rail_info_t topological; /**< Topological rail info */
 } sctk_rail_info_spec_t;
