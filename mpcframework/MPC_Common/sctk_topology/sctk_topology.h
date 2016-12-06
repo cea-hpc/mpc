d57285208c3e5a12525df58140e4c798b6ffc614
Marc Perache <marc.perache@cea.fr>
2016-12-06 15:14:50 +0100

    Remove ctrl M in sctk_topology.h

--- mpcframework/MPC_Common/sctk_topology/sctk_topology.h
+++ mpcframework/MPC_Common/sctk_topology/sctk_topology.h
@@ -35,112 +35,113 @@
 #endif
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
-  /*
-    Numbering rules
+/*
+  Numbering rules
 
-    Core_id are renumbered according to a topological numbering. All threads sharing a
-    cache level must have continuous numbers.
-  */
-
+  Core_id are renumbered according to a topological numbering. All threads
+  sharing a
+  cache level must have continuous numbers.
+*/
 
 /*! \brief Initialize the topology module
 */
-void sctk_topology_init (void);
+void sctk_topology_init(void);
 
 /*! \brief Destroy the topology module
 */
-void sctk_topology_destroy (void);
+void sctk_topology_destroy(void);
 
 /*! \brief Return the current core_id
 */
-int sctk_get_cpu (void);
+int sctk_get_cpu(void);
 
 /*! \brief Return the total number of core for the process
 */
-int sctk_get_cpu_number (void);
+int sctk_get_cpu_number(void);
 
 /*! \brief Return the total number of core for the process (from a topology)
 */
-int sctk_get_cpu_number_topology (hwloc_topology_t topo);
+int sctk_get_cpu_number_topology(hwloc_topology_t topo);
 
 /*! \brief Return 1 if the current node is a NUMA node, 0 otherwise
 */
-int sctk_is_numa_node (void);
+int sctk_is_numa_node(void);
 
-/*! \brief Return 1 if the current node is a NUMA node, 0 otherwise (from a topology)
+/*! \brief Return 1 if the current node is a NUMA node, 0 otherwise (from a
+ * topology)
 */
-int sctk_is_numa_node_topology (hwloc_topology_t topo);
+int sctk_is_numa_node_topology(hwloc_topology_t topo);
 
 /*! \brief Print the topology tree into a file
  * @param fd Destination file descriptor
 */
-void sctk_print_topology (FILE * fd);
+void sctk_print_topology(FILE *fd);
 
 /*! \brief Print a given topology tree into a file
  * @param fd Destination file descriptor
  * @param topology the topology to print
 */
-void sctk_print_specific_topology (FILE * fd, hwloc_topology_t topology);
+void sctk_print_specific_topology(FILE *fd, hwloc_topology_t topology);
 
 /*! \brief Bind the current thread
  * @ param i The cpu_id to bind
 */
-int sctk_bind_to_cpu (int i);
+int sctk_bind_to_cpu(int i);
 
 /*! \brief Return the first core_id for a NUMA node
  * @ param node NUMA node id.
 */
-int sctk_get_first_cpu_in_node (int node);
+int sctk_get_first_cpu_in_node(int node);
 
 /*! \brief Return the type of processor (x86, x86_64, ...)
 */
-char *sctk_get_processor_name (void);
+char *sctk_get_processor_name(void);
 
 /*! \brief Set the number of core usable for the current process
  * @ param n Number of cores
  * used for ethread
 */
-int sctk_set_cpu_number (int n);
+int sctk_set_cpu_number(int n);
 
 /*! \brief Return the hostname
 */
-char *sctk_get_node_name (void);
+char *sctk_get_node_name(void);
 
 /*! \brief Return the closest core_id
  * @param cpuid Main core_id
  * @param nb_cpus Number of neighbor
  * @param neighborhood Neighbor list
 */
-void sctk_get_neighborhood(int cpuid, int nb_cpus, int* neighborhood);
+void sctk_get_neighborhood(int cpuid, int nb_cpus, int *neighborhood);
 
 /*! \brief Return the closest core_id
  * @param cpuid Main core_id
  * @param nb_cpus Number of neighbor
  * @param neighborhood Neighbor list
 */
-void sctk_get_neighborhood_topology(hwloc_topology_t topo, int cpuid, int nb_cpus, int* neighborhood);
+void sctk_get_neighborhood_topology(hwloc_topology_t topo, int cpuid,
+                                    int nb_cpus, int *neighborhood);
 
 /*! \brief Return the number of NUMA nodes
 */
-int sctk_get_numa_node_number (void);
+int sctk_get_numa_node_number(void);
 
 /*! \brief Return the NUMA node according to the code_id number
  * @param vp VP
 */
-int sctk_get_node_from_cpu (const int vp);
+int sctk_get_node_from_cpu(const int vp);
 
 /*! \brief Return the NUMA node according to the code_id number
  * @param vp VP
 */
-int sctk_get_node_from_cpu_topology (hwloc_topology_t topo, const int vp);
+int sctk_get_node_from_cpu_topology(hwloc_topology_t topo, const int vp);
 
 /*! \brief Return the hwloc topology object
 */
-hwloc_topology_t sctk_get_topology_object (void);
+hwloc_topology_t sctk_get_topology_object(void);
 
 void sctk_restrict_binding();
 
@@ -156,18 +157,20 @@ int sctk_restrict_topology_for_mpcomp(hwloc_topology_t *restrictedTopology,
 
 #ifdef MPC_USE_INFINIBAND
 #include <infiniband/verbs.h>
-/*! \brief Return if the core_id is close from the core_id. If an error occurs, we get -1
+/*! \brief Return if the core_id is close from the core_id. If an error occurs,
+ * we get -1
 */
-int sctk_topology_is_ib_device_close_from_cpu (struct ibv_device * dev, int core_id);
+int sctk_topology_is_ib_device_close_from_cpu(struct ibv_device *dev,
+                                              int core_id);
 #endif
 
 /*! \brief Convert a PU id to a logical index
 */
-int sctk_topology_convert_os_pu_to_logical( int pu_os_id );
+int sctk_topology_convert_os_pu_to_logical(int pu_os_id);
 
 /*! \brief Convert an OS PU to a logical index
 */
-int sctk_topology_convert_logical_pu_to_os( int pu_id );
+int sctk_topology_convert_logical_pu_to_os(int pu_id);
 
 /*! \brief Returns the complete allowed cpu set for this process
 */
@@ -176,31 +179,32 @@ hwloc_const_cpuset_t sctk_topology_get_machine_cpuset();
 /*! \brief Return the cpuset of the node containing the PU with pu_id
  * if not numa it behaves like  sctk_topology_get_machine_cpuset
 */
-hwloc_const_cpuset_t sctk_topology_get_numa_cpuset( int pu_id );
+hwloc_const_cpuset_t sctk_topology_get_numa_cpuset(int pu_id);
 
 /*! \brief Return the cpuset of the socket containing the PU with pu_id
 */
-hwloc_cpuset_t sctk_topology_get_socket_cpuset( int pu_id );
+hwloc_cpuset_t sctk_topology_get_socket_cpuset(int pu_id);
 
 /*! \brief Return the cpuset of the PU with pu_id
 */
-hwloc_cpuset_t sctk_topology_get_pu_cpuset( int pu_id );
+hwloc_cpuset_t sctk_topology_get_pu_cpuset(int pu_id);
 
 /*! \brief Return the cpuset of the CORE hosting PU with pu_id
 */
-hwloc_cpuset_t sctk_topology_get_core_cpuset( int pu_id );
+hwloc_cpuset_t sctk_topology_get_core_cpuset(int pu_id);
 
-/*! \brief Retrieve a CPUSET with the first core for each item of level NUMA/MACHINE/SOCKET
+/*! \brief Retrieve a CPUSET with the first core for each item of level
+ * NUMA/MACHINE/SOCKET
 */
-hwloc_cpuset_t sctk_topology_get_roots_for_level( hwloc_obj_type_t type );
+hwloc_cpuset_t sctk_topology_get_roots_for_level(hwloc_obj_type_t type);
 
 /*! \brief Compute the distance between a given object and a PU
 */
-int sctk_topology_distance_from_pu( int source_pu, hwloc_obj_t target_obj );
+int sctk_topology_distance_from_pu(int source_pu, hwloc_obj_t target_obj);
 
 void sctk_topology_init_cpu();
-  
-  int sctk_get_pu_number();
+
+int sctk_get_pu_number();
 #ifdef __cplusplus
 }
 #endif
