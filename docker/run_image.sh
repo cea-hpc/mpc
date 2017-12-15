490ff040feaecac47ae46ea911dd0e5225535e4a
Julien Adam <adamj@paratools.com>
2017-12-15 14:08:05 +0100

    MPC_BUILD: Allow image download from hub.docker.com/paratoolsfrance/mpc-env/

--- docker/run_image.sh
+++ docker/run_image.sh
@@ -3,6 +3,7 @@
 SRC="`readlink -e $0`"
 SRC="`dirname $SRC`"
 UNIQ=
+FROM_NET=no
 MPC_ENV=alpine
 MPC_PATH="$SRC/.."
 
@@ -29,7 +30,8 @@ for arg in "$@"
 do
 	case $arg in
 		img=*)
-			MPC_ENV=`read_param "$arg" "img="`
+			os=`read_param "$arg" "img="`
+			MPC_ENV="mpc-env:$os"
 			;;
 		list)
 			printf "Available images:\n"
@@ -39,6 +41,9 @@ do
 		--uniq)
 			UNIQ="--rm"
 			;;
+		--net)
+			FROM_NET="yes"
+			;;
 		*)
 			die "Unknown arg '$arg'"
 			exit 2
@@ -46,10 +51,20 @@ do
 	esac
 done
 
-test -n "`docker images -q mpc-env:$MPC_ENV`" || die "$MPC_ENV does not exist"
 
-running_container="`docker ps -q -f "ancestor=mpc-env:$MPC_ENV"`"
-container="`docker ps -q -a -f "ancestor=mpc-env:$MPC_ENV"`"
+image=`docker images -q $MPC_ENV`
+if test -z "$image"; then
+	if test "$FROM_NET" = "yes"; then
+		echo "Try to download the latest $MPC_ENV from hub.docker.com repository"
+		docker pull paratoolsfrance/$MPC_ENV || die "$MPC_ENV does not remotely exist"
+		MPC_ENV="paratoolsfrance/$MPC_ENV"
+	else
+		die "Unable to find a proper Docker image. Try to run ./build_image.sh [img=$os | --net ] first"
+	fi
+fi
+
+running_container="`docker ps -q -f "ancestor=$MPC_ENV"`"
+container="`docker ps -q -a -f "ancestor=$MPC_ENV"`"
 check_no_multiple_container "$container"
 
 echo "Building MPC: /opt/src/mpc/installmpc --prefix=/home/mpcuser/install"
@@ -62,7 +77,7 @@ elif test -n "$container"; then
 	echo "Starting from an existing container ($container)"
 	docker start -ai $container
 else
-	echo "Running a new container for mpc-env:$MPC_ENV"
+	echo "Running a new container for $MPC_ENV"
 	test -n "$UNIQ" && echo "** Warning **: The container will be destroyed when exit (can take a while)"
-	docker run $UNIQ -h $MPC_ENV -w /home/mpcuser/build -v $MPC_PATH:/opt/src/mpc -it mpc-env:$MPC_ENV
+	docker run $UNIQ -h $os -w /home/mpcuser/build -v $MPC_PATH:/opt/src/mpc -it $MPC_ENV
 fi
