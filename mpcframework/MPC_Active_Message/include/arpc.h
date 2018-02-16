de1fb3ed8120650fbc303ebc26a962fd690a36b2
Julien Adam <adamj@paratools.com>
2018-02-16 12:28:18 +0100

    MPC_Active_Message: Dummy RPC protocol (through MPI) to test the frontend

--- mpcframework/MPC_Active_Message/include/arpc.h
+++ mpcframework/MPC_Active_Message/include/arpc.h
@@ -1,13 +1,17 @@
 #ifndef __ARPC_H_
 #define __ARPC_H_
+#include <string.h>
 
 struct sctk_arpc_context
 {
 	int dest;
+	int rpcode;
+	int srvcode;
 };
 typedef struct sctk_arpc_context sctk_arpc_context_t;
 
-void* arpc_emit_call(sctk_arpc_context_t* ctx, const void* input);
-void* arpc_recv_call(sctk_arpc_context_t* ctx, const void* input);
+int arpc_emit_call(sctk_arpc_context_t* ctx, const void* request, size_t req_size, void* response, size_t* resp_size);
+int arpc_recv_call(sctk_arpc_context_t* ctx, const void* request, size_t req_size, void* response, size_t* resp_size);
 
+int arpc_polling_request(sctk_arpc_context_t* ctx);
 #endif /* ifndef __ARPC_H_ */
--- mpcframework/MPC_Active_Message/src/sctk_arpc.c
+++ mpcframework/MPC_Active_Message/src/sctk_arpc.c
@@ -1,15 +1,74 @@
 #include <arpc.h>
+#include <mpi.h>
+#include <sctk_allocator.h>
 #include <sctk_debug.h>
 
-void* arpc_emit_call(sctk_arpc_context_t* ctx, const void* input)
-{
-	sctk_warning("Emit an RPC call");
+#define MPI_ARPC_TAG  1000
+#define __arpc_print_ctx(ctx, format,...) do { \
+	sctk_warning("[%d,S:%d,C:%d] "format, ctx->dest,ctx->srvcode, ctx->rpcode, ##__VA_ARGS__);} while(0)
 
-	/* cheat for now */
-	return arpc_recv_call(ctx, input);
+int arpc_emit_call(sctk_arpc_context_t* ctx, const void* input, size_t req_size, void* response, size_t*resp_size)
+{
+	__arpc_print_ctx(ctx, "Send req=%p (sz:%llu) <--> resp=%p (sz: %llu)", input, req_size, response, *resp_size);
+
+	MPI_Send( &ctx->rpcode , 1 , MPI_INT , ctx->dest , MPI_ARPC_TAG , MPI_COMM_WORLD );
+	MPI_Send( &ctx->srvcode , 1 , MPI_INT , ctx->dest , MPI_ARPC_TAG , MPI_COMM_WORLD );
+
+	MPI_Send( &req_size , 1 , MPI_LONG_LONG_INT, ctx->dest , MPI_ARPC_TAG , MPI_COMM_WORLD );
+	
+	if(req_size >0){
+
+		MPI_Send( input , req_size , MPI_BYTE , ctx->dest , MPI_ARPC_TAG , MPI_COMM_WORLD );
+	}
+	MPI_Recv( resp_size , 1 , MPI_LONG_LONG_INT , ctx->dest , MPI_ARPC_TAG , MPI_COMM_WORLD , MPI_STATUS_IGNORE );
+
+	if(*resp_size > 0)
+	{
+		MPI_Recv( response , *(int*)resp_size , MPI_BYTE , ctx->dest , MPI_ARPC_TAG , MPI_COMM_WORLD , MPI_STATUS_IGNORE );
+	}
+
+	return 1;
 }
 
-void* arpc_recv_call(sctk_arpc_context_t* ctx, const void* input)
+int arpc_recv_call(sctk_arpc_context_t* ctx, const void* input, size_t req_size, void* response, size_t* resp_size)
 {
-	return NULL;
+	__arpc_print_ctx(ctx, "Recv req=%p (sz:%llu) <--> resp=%p (sz: %llu)", input, req_size, response, *resp_size);
+
+
+	
+	return 1;
+}
+
+
+int arpc_polling_request(sctk_arpc_context_t* ctx)
+{
+	void * request = NULL;
+	size_t req_size = 0;
+	void * response = NULL;
+	size_t resp_size = 0;
+
+
+	int i = 0;
+	MPI_Recv( &ctx->rpcode , 1 , MPI_INT , ctx->dest , MPI_ARPC_TAG , MPI_COMM_WORLD , MPI_STATUS_IGNORE );
+	MPI_Recv( &ctx->srvcode , 1 , MPI_INT , ctx->dest , MPI_ARPC_TAG , MPI_COMM_WORLD , MPI_STATUS_IGNORE );
+
+	MPI_Recv( &req_size , 1 , MPI_LONG_LONG_INT , ctx->dest , MPI_ARPC_TAG , MPI_COMM_WORLD , MPI_STATUS_IGNORE );
+	if(req_size > 0)
+	{
+		request = malloc(req_size);
+		MPI_Recv( request , req_size, MPI_BYTE , ctx->dest , MPI_ARPC_TAG , MPI_COMM_WORLD , MPI_STATUS_IGNORE );
+	}
+	
+	arpc_recv_call(ctx, request, req_size, &response, &resp_size);
+	
+	response =malloc(resp_size);
+
+	MPI_Send( &resp_size , 1 , MPI_LONG_LONG_INT , ctx->dest , MPI_ARPC_TAG , MPI_COMM_WORLD );
+	
+	if(resp_size > 0)
+	{
+		MPI_Send( response , resp_size , MPI_BYTE , ctx->dest , MPI_ARPC_TAG , MPI_COMM_WORLD );
+	}
+
+	return 0;
 }
