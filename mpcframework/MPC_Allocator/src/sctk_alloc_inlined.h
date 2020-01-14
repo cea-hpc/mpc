23f64ea52f958afa1c77642461b37bc0a2a2e278
Julien Adam <adamj@paratools.com>
2020-01-14 12:38:30 +0100

    MPC_Allocator: Remove macro not defined when compiled standalone

--- mpcframework/MPC_Allocator/src/sctk_alloc_inlined.h
+++ mpcframework/MPC_Allocator/src/sctk_alloc_inlined.h
@@ -180,8 +180,6 @@ static __inline__ sctk_alloc_vchunk sctk_alloc_get_chunk(sctk_addr_t ptr)
 	//check type and magik number
 	if (vchunk->unused_magik != SCTK_ALLOC_MAGIC_STATUS)
 	{
-
-	CRASH();
 		SCTK_PDEBUG("Bad address is %p.",ptr);
 		warning("Header content error while trying to find chunk header.");
                 warning("Did you free a system malloc chunk with MPC ?");
