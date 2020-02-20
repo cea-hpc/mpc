6cf8cc3d1949321810ae2bff30bde29e0b9932ee
Jean-Baptiste Besnard <jbbesnard@paratools.com>
2020-02-20 08:36:47 +0100

    ROOTINSTALL: remove deps which are now embeded

--- sctk_arch/libcontext/GEN/sctk_ucontext.h
+++
@@ -1,9 +0,0 @@
-#include <ucontext.h>
-
-typedef ucontext_t sctk_ucontext_t;
-
-#define mpc__makecontext makecontext
-#define mpc__setcontext setcontext
-#define mpc__getcontext getcontext
-#define mpc__swapcontext swapcontext
-
