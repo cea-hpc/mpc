d47dbea36a2172cb6055c2333d21488f4c6f62a1
Jean-Baptiste Besnard <jbbesnard@paratools.com>
2020-04-08 13:36:23 +0200

    ROMIO: remove uneeded files

--- src/MPC_IO/src/romio/autogen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-${AUTORECONF:-autoreconf} ${autoreconf_args:-"-vif"} -I confdb
-
-echo "=== running autoreconf in 'mpl' ==="
-(cd mpl && ${AUTORECONF:-autoreconf} ${autoreconf_args:-"-vif"}) || exit 1
--- src/MPC_IO/src/romio/mpi-io/Makefile.mk
+++
@@ -1,6 +0,0 @@
-## -*- Mode: Makefile; -*-
-## vim: set ft=automake :
-##
-## (C) 2011 by Argonne National Laboratory.
-##     See COPYRIGHT in top-level directory.
-##
