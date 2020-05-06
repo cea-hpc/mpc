cba3833bacacb602d636208c83ee20ea069d9209
Julien Adam <adamj@paratools.com>
2020-05-06 10:22:50 +0200

    CHORES: enforce strict build tool version through autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -23,4 +23,44 @@
 #                                                                      #
 ########################################################################
 
-autoreconf -vi
+ac_expect="2.69"
+am_expect="1.15"
+lt_expect="2.4.6"
+
+ret=0
+err()
+{
+	1>&2 printf "Error: $@\n"
+	ret=1
+}
+
+extract_version()
+{
+	"$1" --version | head -n 1 | grep -E -o "[0-9]+(\.[0-9]+)*"
+}
+
+arc=$(command -v autoreconf)
+ac=$(command -v autoconf)
+am=$(command -v automake)
+lt=$(command -v libtoolize)
+
+test -n "$arc" || err "No 'autoreconf' command in PATH. Please install autoconf first."
+test -n "$ac" || err "No 'autoconf' command in PATH. Please install autoconf first."
+test -n "$am" || err "No 'automake' command in PATH. Please install automake first."
+test -n "$lt" || err "No 'libtoolize' command in PATH. Please install libtool first."
+
+
+ac_version=$(extract_version "$ac")
+am_version=$(extract_version "$am")
+lt_version=$(extract_version "$lt")
+
+
+test "$ac_version" = "$ac_expect" || err "Exact version required for Autoconf: $ac_expect (got $ac_version)"
+test "$am_version" = "$am_expect" || err "Exact version required for Automake: $am_expect (got $am_version)"
+test "$lt_version" = "$lt_expect" || err "Exact version required for Autoconf: $lt_expect (got $lt_version)"
+
+if test "$ret" = "0"; then
+	$arc -vi
+	ret=$?
+fi
+test "$ret" = "0" || printf "Abort configuration due to error(s) above.\n"
