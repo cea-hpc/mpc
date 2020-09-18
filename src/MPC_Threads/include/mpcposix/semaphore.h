9a1bba0ab4ea435f9ad5c9176f1313361213327b
Jean-Baptiste Besnard <jbbesnard@paratools.com>
2020-09-18 06:54:02 +0000

    SEMAPHORE: install redirect to pthread

---
+++ src/MPC_Threads/include/mpcposix/semaphore.h
@@ -0,0 +1,8 @@
+#ifndef MPC_SEMAPHORE
+#define MPC_SEMAPHORE
+
+/* This is a redirect for MPC to load the
+   pthread rewriting interfafe first */
+#include <pthread.h>
+
+#endif /* MPC_SEMAPHORE */
