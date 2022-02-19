5a470d5bfc27cc69f1c8ec443b0e99666882c31a
Romain PEREIRA <romain.pereira.ocre@cea.fr>
2022-02-19 15:41:05 +0100

    ajout MPC_OMP_TASK_SET_LABEL macro

---
+++ src/MPC_OpenMP/include/mpc_omp_task_label.h
@@ -0,0 +1,12 @@
+#ifndef MPC_OMP_TASK_LABEL_H
+# define MPC_OMP_TASK_LABEL_H
+
+# include <mpc_omp.h>
+
+char MPC_OMP_TASK_LABEL_BUFFER_NAME[MPC_OMP_TASK_LABEL_MAX_LENGTH];
+#  define MPC_OMP_TASK_SET_LABEL(...)    do {\
+    snprintf(MPC_OMP_TASK_LABEL_BUFFER_NAME, MPC_OMP_TASK_LABEL_MAX_LENGTH, __VA_ARGS__);           \
+    mpc_omp_task_label(MPC_OMP_TASK_LABEL_BUFFER_NAME);                                             \
+} while(0)
+
+#endif
