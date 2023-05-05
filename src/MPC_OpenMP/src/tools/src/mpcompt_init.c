51017ef69626c855670203f3e18bfa14596e25cc
Thomas Dionisi <thomas.dionisi@uvsq.fr>
2023-05-05 17:55:44 +0200

    OMPT : Bugfix OMP_TOOL_LIBRARIES environment variable

--- src/MPC_OpenMP/src/tools/src/mpcompt_init.c
+++ src/MPC_OpenMP/src/tools/src/mpcompt_init.c
@@ -51,12 +51,16 @@ __get_start_tool_result ( unsigned int omp_version,
                       "record tool path" : "no tool path record", tool_path );
 
     /* Is omp_start_tool routine defined in address space? */
+
     ret = ompt_start_tool( omp_version, runtime_version );
 
     /* Otherwise, try to find ompt_start_tool routine in one of candidate tools 
      * provided by user OMP_TOOL_LIBRARIES environment variable */
-    if( !ret && mpcomp_global_icvs.tool_libraries ) {
-        tool_libraries = strdup( mpcomp_global_icvs.tool_libraries );
+    const char* tool_libraries_env = NULL;
+    tool_libraries_env = getenv("OMP_TOOL_LIBRARIES") ;
+
+    if( !ret && tool_libraries_env ) {
+        tool_libraries = strdup( tool_libraries_env );
         candidate_tool_path = strtok_r( tool_libraries, ":", &save_ptr );
 
         while( !ret && candidate_tool_path ) {
