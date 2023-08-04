191e5d38b1f8b006bb5e854df29943cf80ca3491
Paul Canat <pcanat@paratools.com>
2023-08-04 15:04:13 +0200

    MEMPOOL: Doxygen inline documentation for mempool

--- src/MPC_Common/include/mpc_mempool.h
+++ src/MPC_Common/include/mpc_mempool.h
@@ -23,8 +23,24 @@ typedef struct mpc_mempool_s {
     size_t size;
 } mpc_mempool;
 
+/**
+ * @brief Add a buffer to the available buffers stack.
+ * 
+ * @param buf buffer to stack
+ */
 void _mpc_mempool_stack(void * buf);
 
+/**
+ * @brief Initialize a mempool.
+ * 
+ * @param mp pointer to mempool to be initialized. Must be allocated to `sizeof(mpc_mempool)`
+ * @param min minimum number of used or available buffers (do not free under this number)
+ * @param max maximum number of available buffers (systematically free over this number)
+ * @param size buffers size
+ * @param malloc_func function used to malloc buffers. Must be `void *func(int size)`
+ * @param free_func function used to free buffers. Must be `int func(void *buffer)`
+ * @return int MPC_SUCCESS in case of success
+ */
 int mpc_mempool_init(mpc_mempool *mp, 
     int min, 
     int max, 
@@ -32,12 +48,41 @@ int mpc_mempool_init(mpc_mempool *mp,
     void *(*malloc_func)(size_t), 
     void (*free_func)(void *));
 
+/**
+ * @brief Allocate a fixed size buffer.
+ * 
+ * @param mempool mempool used to allocate the buffer
+ * @return void* allocated buffer
+ */
 void *mpc_mempool_alloc(mpc_mempool *mempool);
 
+/**
+ * @brief Free a buffer allocated with a mpc_mempool.
+ * 
+ * @param mempool mempool used to allocate the buffer
+ * @param buffer buffer to be freed
+ */
 void mpc_mempool_free(mpc_mempool *mempool, void *buffer);
 
+/**
+ * @brief Empty a mempool.
+ * 
+ * @param mp mempool to be emptied
+ * @return int MPC_SUCCESS in case of success
+ */
 int mpc_mempool_empty(mpc_mempool *mp);
 
+/**
+ * @brief Allocate a buffer. Initialize the mempool if it is not initialized.
+ * 
+ * @param mp mempool used to allocate the buffer
+ * @param min minimum number of used or available buffers (do not free under this number)
+ * @param max maximum number of available buffers (systematically free over this number)
+ * @param size buffers size
+ * @param malloc_func function used to malloc buffers. Must be `void *func(int size)`
+ * @param free_func function used to free buffers. Must be `int func(void *buffer)`
+ * @return void* allocated buffer
+ */
 void *mpc_mempool_alloc_and_init(mpc_mempool *mp, 
                             int min, 
                                 int max, 
