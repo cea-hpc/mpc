44e9dbc0a596a3bf5650fbfe08e65b37c4dc3d7f
Rémi Dehenne <remi.dehenne@cea.fr>
2024-06-13 14:59:36 +0200

    ARCH: Mark vars as unused in generic microtask
    N/A

--- src/MPC_Arch/src/microtask/generic/microtask.c
+++ src/MPC_Arch/src/microtask/generic/microtask.c
@@ -19,6 +19,7 @@
 /* #   - CARRIBAULT patrick patrick.carribault@cea.fr                     # */
 /* #                                                                      # */
 /* ######################################################################## */
+#include "src/MPC_Common/include/mpc_keywords.h"
 #include <assert.h>
 //#include "sctk_microtask.h"
 //#include "mpc_arch.h"
@@ -104,27 +105,27 @@ __kmp_invoke_microtask(microtask_t pkfn, int gtid, int tid, int argc, void *p_ar
 //   assert(0);
 // }
 
-kmp_int32  __kmp_test_then_add32( volatile kmp_int32 *a, kmp_int32  b) {assert(0);}
-kmp_int64  __kmp_test_then_add64( volatile kmp_int64 *a, kmp_int64 b) {assert(0);}
+kmp_int32  __kmp_test_then_add32( volatile __UNUSED__ kmp_int32 *a, __UNUSED__ kmp_int32  b) {assert(0);}
+kmp_int64  __kmp_test_then_add64( volatile __UNUSED__ kmp_int64 *a, __UNUSED__ kmp_int64 b) {assert(0);}
 
-kmp_int8   __kmp_compare_and_store8( volatile kmp_int8 *a, kmp_int8 b, kmp_int8 c){assert(0);}
-kmp_int16  __kmp_compare_and_store16( volatile kmp_int16 *a, kmp_int16 b, kmp_int16 c){assert(0);}
-kmp_int32  __kmp_compare_and_store32( volatile kmp_int32 *a, kmp_int32 b, kmp_int32 c){assert(0);}
-kmp_int32  __kmp_compare_and_store64( volatile kmp_int64 *a, kmp_int64 b, kmp_int64 c){assert(0);}
-kmp_int8   __kmp_compare_and_store_ret8( volatile kmp_int8  *p, kmp_int8  cv, kmp_int8  sv ){assert(0);}
-kmp_int16  __kmp_compare_and_store_ret16( volatile kmp_int16 *p, kmp_int16 cv, kmp_int16 sv ){assert(0);}
-kmp_int32  __kmp_compare_and_store_ret32( volatile kmp_int32 *p, kmp_int32 cv, kmp_int32 sv ){assert(0);}
-kmp_int64  __kmp_compare_and_store_ret64( volatile kmp_int64 *p, kmp_int64 cv, kmp_int64 sv ){assert(0);}
+kmp_int8   __kmp_compare_and_store8( volatile __UNUSED__ kmp_int8 *a, __UNUSED__ kmp_int8 b, __UNUSED__ kmp_int8 c){assert(0);}
+kmp_int16  __kmp_compare_and_store16( volatile __UNUSED__ kmp_int16 *a, __UNUSED__ kmp_int16 b, __UNUSED__ kmp_int16 c){assert(0);}
+kmp_int32  __kmp_compare_and_store32( volatile __UNUSED__ kmp_int32 *a, __UNUSED__ kmp_int32 b, __UNUSED__ kmp_int32 c){assert(0);}
+kmp_int32  __kmp_compare_and_store64( volatile __UNUSED__ kmp_int64 *a, __UNUSED__ kmp_int64 b, __UNUSED__ kmp_int64 c){assert(0);}
+kmp_int8   __kmp_compare_and_store_ret8( volatile __UNUSED__ kmp_int8  *p, __UNUSED__ kmp_int8  cv, __UNUSED__ kmp_int8  sv ){assert(0);}
+kmp_int16  __kmp_compare_and_store_ret16( volatile __UNUSED__ kmp_int16 *p, __UNUSED__ kmp_int16 cv, __UNUSED__ kmp_int16 sv ){assert(0);}
+kmp_int32  __kmp_compare_and_store_ret32( volatile __UNUSED__ kmp_int32 *p, __UNUSED__ kmp_int32 cv, __UNUSED__ kmp_int32 sv ){assert(0);}
+kmp_int64  __kmp_compare_and_store_ret64( volatile __UNUSED__ kmp_int64 *p, __UNUSED__ kmp_int64 cv, __UNUSED__ kmp_int64 sv ){assert(0);}
 
-kmp_int8   __kmp_xchg_fixed8( volatile kmp_int8  *p, kmp_int8  v ){assert(0);}
-kmp_int16  __kmp_xchg_fixed16( volatile kmp_int16 *p, kmp_int16 v ){assert(0);}
-kmp_int32  __kmp_xchg_fixed32( volatile kmp_int32 *p, kmp_int32  v) {assert(0);}
-kmp_int64  __kmp_xchg_fixed64( volatile kmp_int64 *p, kmp_int64  v) {assert(0);}
-kmp_real32 __kmp_xchg_real32( volatile kmp_real32 *p, kmp_real32 v ){assert(0);}
-kmp_real64 __kmp_xchg_real64( volatile kmp_real64 *p, kmp_real64 v ){assert(0);}
+kmp_int8   __kmp_xchg_fixed8( volatile __UNUSED__ kmp_int8  *p, __UNUSED__ kmp_int8  v ){assert(0);}
+kmp_int16  __kmp_xchg_fixed16( volatile __UNUSED__ kmp_int16 *p, __UNUSED__ kmp_int16 v ){assert(0);}
+kmp_int32  __kmp_xchg_fixed32( volatile __UNUSED__ kmp_int32 *p, __UNUSED__ kmp_int32  v) {assert(0);}
+kmp_int64  __kmp_xchg_fixed64( volatile __UNUSED__ kmp_int64 *p, __UNUSED__ kmp_int64  v) {assert(0);}
+kmp_real32 __kmp_xchg_real32( volatile __UNUSED__ kmp_real32 *p, __UNUSED__ kmp_real32 v ){assert(0);}
+kmp_real64 __kmp_xchg_real64( volatile __UNUSED__ kmp_real64 *p, __UNUSED__ kmp_real64 v ){assert(0);}
 
-double     __kmp_test_then_add_real32( kmp_real32 *a, kmp_real32 b){assert(0);}
-double     __kmp_test_then_add_real64( kmp_real64 *a, kmp_real64 b){assert(0);}
+double     __kmp_test_then_add_real32( __UNUSED__ kmp_real32 *a, __UNUSED__ kmp_real32 b){assert(0);}
+double     __kmp_test_then_add_real64( __UNUSED__ kmp_real64 *a, __UNUSED__ kmp_real64 b){assert(0);}
 
 extern  void __sctk_cpu_relax ();
 int __kmp_x86_pause(){sctk_cpu_relax(); return 0;}
