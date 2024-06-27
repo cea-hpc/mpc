1c580502e436b07263d40156af9a3c9ff29080f5
pcanat <canat.paul@gmail.com>
2024-06-27 15:00:28 +0200

    READTHEDOCS: add mpc logo

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -25,4 +25,10 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_logo = "../../img/mpc_logo.png"
 html_static_path = ['_static']
+
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
