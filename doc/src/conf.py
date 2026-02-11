5b7b91a3def256261a93d1cf49c507f551393fd5
Corentin Beaulieu <corentin.beaulieu@cea.fr>
2026-02-11 09:44:20 +0100

    chore(doc): refactor the main doc configuration
    1. The version is automatically generated. The version should be stored
       in only one place as duplication leads to incorrect numbering
    2. The year used for the copyright is automatically generated from the
       date of the generation of the documentation to avoid any omission
    3. The CEA name has been written in full letters to match PCVS
       documentation
    4. The logo is also used as a favicon for the documentation tabs when
       viewed in a browser
    5. The `display_version` option have been removed as it is not supported
       by RTD and provoked a warning at each build

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -6,37 +6,43 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'MPC'
-copyright = '2025, CEA'
-author = 'CEA'
-release = '4.3'
+import subprocess
+from datetime import date
+
+current_year = date.today().year
+
+project = "MPC"
+author = "Commissariat à l'Énergie Atomique et aux Énergies Alternatives"
+copyright = str(current_year) + ", " + author
+version = subprocess.run(["../../utils/get_version"], text=True, capture_output=True).stdout
+release = version
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme', 'breathe', 'myst_parser']
+extensions = ["sphinx_rtd_theme", "breathe", "myst_parser"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-breathe_projects = {'MPC': '../build/doxygen/xml'}
-breathe_default_project = 'MPC'
+breathe_projects = {"MPC": "../build/doxygen/xml"}
+breathe_default_project = "MPC"
 breathe_default_members = ("members", "undoc-members")
 breathe_domain_by_extension = {"h": "c", "c": "c"}
 breathe_show_define_initializer = True
 breathe_show_enumvalue_initializer = True
 breathe_show_include = False
 
-
+pygments_style = "sphinx"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 html_logo = "_static/mpc_logo.png"
-html_static_path = ['_static']
+html_favicon = "_static/mpc_logo.png"
+html_static_path = ["_static"]
 
 html_theme_options = {
-    'logo_only': True,
-    'display_version': False,
+    "logo_only": True,
 }
